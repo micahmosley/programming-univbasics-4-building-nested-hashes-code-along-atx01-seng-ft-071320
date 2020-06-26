b6b63edaa047a3cda560479aeec524383d36ab9c
Micah Mosley <micah.mosley@utexas.edu>
2020-06-26 02:44:52 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -18,14 +18,17 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
+        name: "Lord Capulet",
+        age: "50"
+        },
+      :matriarch => {
         name: "Lady Capulet",
         age: "51"
         },
-      :matriarch => {
-        #your key/value pairs here
-        },
       :heroine => {
-        #your key/value pairs here
+        name: "Juliet",
+        age: "15",
+        status: "alive"
         },
       :heroine_friends => []
    }
